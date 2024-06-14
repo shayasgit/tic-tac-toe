004baaf5916b8d82c9d09c01dda4be7f6744b761
Shayas R.A <shayas.contact@gmail.com>
2024-06-14 22:12:34 +0530

    GameBoard Created

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
-console.log('Tic Tac Toe')
+console.log("Tic Tac Toe");
+
+function gameBoard() {
+  const row = 3;
+  const col = 3;
+  let board = [];
+  for (let i = 0; i < row; i++) {
+    board[i] = [];
+    for (let j = 0; j < col; j++) {
+      board[i].push(null);
+    }
+  }
+  const getBoard = () => board;
+
+  return {getBoard}
+}
+
+let consoleBoard = gameBoard();
